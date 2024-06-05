77a03a8fdcd297d9baab8e94c139b3461220b04c
ThinhDola999 <nguyenvanthinh228866@gmail.com>
2024-06-05 21:59:57 +0700

    them blog, portfolio

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+
+
+    const toggleDarkModeButton = $('#toggleDarkMode');
+    const body = $('body');
+
+
+    let isDarkMode = false;
+    let StringIsDarkMode = "Dart";
+    function updateDarkModeState() {
+        // Lưu trạng thái chế độ tối vào localStorage
+        localStorage.setItem('isDarkModeEnabled', StringIsDarkMode);
+    }
+
+    const localStorageIsDarkMode = localStorage.getItem('isDarkModeEnabled');
+    // const isDarkMode = getDarkModeState();
+
+    if (localStorageIsDarkMode == "Dark") {
+
+        body.removeClass('light-mode');
+        body.addClass('dart-mode');
+        isDarkMode = false;
+        $('#portfolio-link').addClass('clicked_toi');
+
+    } else if (localStorageIsDarkMode == "Light") {
+
+        body.removeClass('dart-mode');
+        body.addClass('light-mode');
+        isDarkMode = true;
+        $('#portfolio-link').addClass('clicked_sang');
+
+    }
+
+    $('body').css('display', 'block');
+
+
+
+    function createStar() {
+        const star = $('<div>').addClass('star');
+
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * window.innerHeight;
+        const duration = Math.random() * 2 + 0.5; // Thời gian lấp lánh từ 0.5 đến 2 giây
+
+        star.css({
+            left: `${x}px`,
+            top: `${y}px`,
+            animationDuration: `${duration}s`
+        });
+
+        $('#stars').append(star);
+    }
+
+    // Tạo danh sách ngôi sao
+    function createStars() {
+        for (let i = 0; i < 200; i++) {
+            createStar();
+        }
+    }
+
+    // Khởi tạo
+    createStars();
+
+
+
+    toggleDarkModeButton.on('click', function () {
+
+
+        isDarkMode = !isDarkMode;
+
+
+        if (isDarkMode) {
+
+            body.removeClass('dart-mode');
+            body.addClass('light-mode');
+            StringIsDarkMode = "Light";
+            updateDarkModeState();
+            $('#portfolio-link').addClass('clicked_sang');
+
+        } else {
+
+            body.removeClass('light-mode');
+            body.addClass('dart-mode');
+            StringIsDarkMode = "Dark";
+            updateDarkModeState();
+            $('#portfolio-link').addClass('clicked_toi');
+
+        }
+    });
+});
